698b420d7887bf96f321f19a0a26ca4f6e9cb033
Armando Pisfil Puemape <armandoaepp@hotmail.com>
2013-06-18 12:22:32 -0500

    Modal Bootstrap agregado a ivory 18/06/2013 version 0.2

--- bootstrap-modal/form.php
+++ bootstrap-modal/form.php
@@ -1,7 +1,17 @@
-    <form method="post" action='' name="login_form">
-      <p><input type="text" class="span3" name="eid" id="email" placeholder="Email"></p>
-      <p><input type="password" class="span3" name="passwd" placeholder="Password"></p>
-      <p><button type="submit" class="btn btn-primary">Sign in</button>
+    <form method="post" action='' name="login_form" class="hform">
+<fieldset>	
+     <label>Email sdfgw5rhsth sdfhsfgsgfhdfg </label>
+      <input type="text" class="span3" name="eid" id="email" placeholder="Email">
+</fieldset>
+<fieldset>
+      <label>Password</label>
+     <input type="password" class="span3" name="passwd" placeholder="Password"></fieldset>
+
+     <label>Mensaje gsdfgsdfg</label>
+     <textarea></textarea>
+<fieldset>
+     <button type="submit" class="btn btn-primary">Sign in</button>
         <a href="#">Forgot Password?</a>
-      </p>
+  </fieldset>
+    
     </form>
