7161387bbad0e668beb93ad7205f2572b73cca91
Armando Pisfil Puemape <armandoaepp@hotmail.com>
2013-06-18 09:53:44 -0500

    Modal con Bootstrap externo al 18/06/2013 version 0.1

---
+++ bootstrap-modal/form.php
@@ -0,0 +1,7 @@
+    <form method="post" action='' name="login_form">
+      <p><input type="text" class="span3" name="eid" id="email" placeholder="Email"></p>
+      <p><input type="password" class="span3" name="passwd" placeholder="Password"></p>
+      <p><button type="submit" class="btn btn-primary">Sign in</button>
+        <a href="#">Forgot Password?</a>
+      </p>
+    </form>
